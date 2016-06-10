906343cbebfef0124838c615b8232de1127a264d
Derrick Tennant <derrick.tennant@automattic.com>
2016-06-10 00:06:39 -0400

    General code cleanup
    * Removed unnecessary class properties
    * Added filter for necessary user capability
    * Whitespace Fixes
    * Removed debugging code
    * A few small code refactors and fixes
    * Get out of here TimThumb!

--- add-new-default-avatar-emrikols-fork.php
+++ add-new-default-avatar-emrikols-fork.php
@@ -22,33 +22,6 @@ if ( ! class_exists( 'DWS_ANDA' ) ) {
 	class DWS_ANDA {
 
 		/**
-		 * Plugin Slug.
-		 *
-		 * @since 2.0.0
-		 * @access public
-		 * @var string $slug
-		 */
-		var $slug = 'anda';
-
-		/**
-		 * Plugin Name.
-		 *
-		 * @since 2.0.0
-		 * @access public
-		 * @var string $name
-		 */
-		var $name = 'Add New Default Avatar';
-
-		/**
-		 * Plugin access level.
-		 *
-		 * @since 2.0.0
-		 * @access public
-		 * @var string $access
-		 */
-		var $access = 'manage_options';
-
-		/**
 		 * Plugin version.
 		 *
 		 * @since 2.0.0
@@ -80,7 +53,8 @@ if ( ! class_exists( 'DWS_ANDA' ) ) {
 		 */
 		function action_admin_menu() {
 			// Add "Add New Avatar" link under the "Appearance" menu.
-			$page = add_submenu_page( 'themes.php', esc_html__( 'Add New Default Avatar', 'dws' ), esc_html__( 'Add New Avatar', 'dws' ), $this->access, 'add-new-default-avatar', array( $this, 'plugin_page' ) );
+			$user_capability = apply_filters( 'dws_anda_user_capability', 'manage_options' );
+			$page = add_submenu_page( 'themes.php', esc_html__( 'Add New Default Avatar', 'dws' ), esc_html__( 'Add New Avatar', 'dws' ), $user_capability, 'add-new-default-avatar', array( $this, 'plugin_page' ) );
 		}
 
 		/**
@@ -91,7 +65,7 @@ if ( ! class_exists( 'DWS_ANDA' ) ) {
 		function action_admin_enqueue_scripts() {
 			// Enqueue core media uploader.
 			wp_enqueue_media();
-			
+
 			// Plugin CSS.
 			wp_enqueue_style( 'dws_anda_style', plugins_url( 'css/style.css', __FILE__ ), array(), $this->ver, 'all' );  // Add CSS.
 		}
@@ -108,22 +82,23 @@ if ( ! class_exists( 'DWS_ANDA' ) ) {
 					var media_uploader = null;
 
 					function open_media_uploader_image() {
-						media_uploader = wp.media({
-						frame:    "post",
-						state:    "insert",
-						multiple: false
+						media_uploader = wp.media( {
+							frame: 'post',
+							state: 'insert',
+							multiple: false
 						} );
 
-						media_uploader.on( "insert", function() {
-							var json = media_uploader.state().get("selection").first().toJSON();
+						media_uploader.on( 'insert', function() {
+							var json = media_uploader.state().get( 'selection' ).first().toJSON();
 
-							console.log( json );
 							var image_url = json.url;
 							var image_caption = json.caption;
 							var image_title = json.title;
+							var image_id = json.id;
 
 							$( '#dws_anda_image_url_upload' ).val( image_url );
 							$( '#dws_anda_avatar_name' ).val( image_title );
+							$( '#dws_anda_avatar_id' ).val( image_id );
 						} );
 
 						media_uploader.open();
@@ -147,19 +122,20 @@ if ( ! class_exists( 'DWS_ANDA' ) ) {
 			// Add plugin to avatar settings.
 			$options = get_option( 'dws_anda' );
 
-			if ( $options ) {
-				$thumb_url = plugins_url( 'includes/timthumb.php', __FILE__ );
+			if ( false !== $options ) {
 				$anda_avatars = array();
 
 				foreach ( $options['avatars'] as $avatar ) {
-					$image_url = $thumb_url . '?src=' . $avatar['local'];
-					$anda_avatars[ $image_url ] = $avatar['name'];
+					$attachment_data = wp_get_attachment_image_src( $avatar['id'] );
+					if ( false !== $attachment_data ) {
+						$anda_avatars[ $attachment_data[0] ] = esc_html( $avatar['name'] );
+					}
 				}
 
-				return array_merge( $anda_avatars, $avatar_defaults ); // Put our custom avatars on top.
-			} else {
-				return $avatar_defaults;
+				$avatar_defaults = array_merge( $anda_avatars, $avatar_defaults ); // Put our custom avatars on top.
 			}
+
+			return $avatar_defaults;
 		}
 
 		/**
@@ -168,10 +144,10 @@ if ( ! class_exists( 'DWS_ANDA' ) ) {
 		 * @since 2.0.0
 		 */
 		function plugin_construct() {
-			$options = array();											// Set up options array.
-			$options['avatars'] = array();								// Set up avatars array.
-			$options['avatar_default'] = get_option( 'avatar_default' );	// Be sure to save original avatar in case user removes plugin.
-			update_option( 'dws_anda', $options );							// Save plugin options.
+			$options = array();
+			$options['avatars'] = array();
+			$options['avatar_default'] = get_option( 'avatar_default' );
+			update_option( 'dws_anda', $options );
 		}
 
 		/**
@@ -180,11 +156,13 @@ if ( ! class_exists( 'DWS_ANDA' ) ) {
 		 * @since 2.0.0
 		 */
 		function plugin_destruct() {
-			$options = get_option( 'dws_anda' );	// Get Plugin Prefrences.
+			$options = get_option( 'dws_anda' );
 
-			// Change default avatar back to what it was before the plugin was activated.
-			update_option( 'avatar_default', $options['avatar_default'] );
-			delete_option( 'dws_anda' );			// Delete the plugin prefrences.
+			if ( false !== $options ) {
+				// Change default avatar back to what it was before the plugin was activated.
+				update_option( 'avatar_default', $options['avatar_default'] );
+				delete_option( 'dws_anda' );
+			}
 		}
 
 		/**
@@ -203,10 +181,10 @@ if ( ! class_exists( 'DWS_ANDA' ) ) {
 				foreach ( $options['avatars'] as $avatar ) {
 					if ( isset( $_POST['dws_anda_delete'] ) && ! in_array( $avatar['uid'], wp_unslash( $_POST['dws_anda_delete'] ), true ) ) { // WPCS: input var okay.
 						$updated_avatars[] = array(
-							'local' => $avatar['local'],
 							'url' => $avatar['url'],
 							'name' => $avatar['name'],
 							'uid' => $avatar['uid'],
+							'id' => $avatar['id'],
 						);
 					}
 				}
@@ -214,15 +192,15 @@ if ( ! class_exists( 'DWS_ANDA' ) ) {
 				update_option( 'dws_anda', $options );
 			}
 
-			if ( isset( $_POST['new'] ) && isset( $_POST['dws_anda_localfile'] ) && isset( $_POST['dws_anda_image_url'] ) ) { // WPCS: input var okay.
+			if ( isset( $_POST['new'] && isset( $_POST['dws_anda_image_url'] ) ) ) { // WPCS: input var okay.
 				// Safety check.  Did the admin do this?
 				check_admin_referer( 'dws_anda_new' );
 
 				$avatar = array();
-				$avatar['local'] = sanitize_text_field( wp_unslash( $_POST['dws_anda_localfile'] ) ); // WPCS: input var okay.
 				$avatar['url'] = esc_url_raw( sanitize_text_field( wp_unslash( $_POST['dws_anda_image_url'] ) ) ); // WPCS: input var okay.
 				$avatar['uid'] = 'DWS' . md5( uniqid() );
 				$avatar['name'] = isset( $_POST['dws_anda_avatar_name'] ) ? sanitize_text_field( wp_unslash( $_POST['dws_anda_avatar_name'] ) ) : $avatar['uid']; // WPCS: input var okay.
+				$avatar['id'] = isset( $_POST['dws_anda_avatar_id'] ) ? absint( $_POST['dws_anda_avatar_id'] ) : 0; // WPCS: input var okay.
 
 				$options['avatars'][] = $avatar;
 
@@ -240,7 +218,7 @@ if ( ! class_exists( 'DWS_ANDA' ) ) {
 			<div class="wrap">
 				<div id="icon-upload" class="icon32"></div>
 				<h2><?php esc_html_e( 'Custom Default Avatars', 'dws' ); ?></h2>
-				
+
 				<form id="dws_anda_add" method="post">
 					<fieldset>
 						<ul>
@@ -253,56 +231,40 @@ if ( ! class_exists( 'DWS_ANDA' ) ) {
 								<label for="dws_anda_image_url"><?php esc_html_e( 'Image URL', 'dws' ); ?>: </label>
 								<input class='text' name='dws_anda_image_url' id='dws_anda_image_url_upload' type='text' value='' />
 								<input type="button" class="button button-primary button-large" id='dws_anda_image_url' value="Select Image" />
+								<input type="hidden" id="dws_anda_avatar_id" name="dws_anda_avatar_id" />
 							</li>
 							<li class='nothidden'>
 								<label for="dws_anda_avatar_name"><?php esc_html_e( 'Avatar Name', 'dws' ); ?>: </label>
 								<input type="text" class="text" id="dws_anda_avatar_name" name="dws_anda_avatar_name" id="dws_anda_avatar_name" value="" />
 							</li>
 							<li class='nothidden'>
-								<input type="submit" class="save" value="<?php esc_html_e( 'Add Avatar', 'dws' ); ?>" />
+								<?php submit_button( esc_html__( 'Add Avatar', 'dws' ), 'secondary' ); ?>
 							</li>
 						</ul>
 					</fieldset>
 				</form>
-				<?php $this->show_avatars(); ?>
-			</div>
-			<?php
-		}
-
-		/**
-		 * Show Avatars.
-		 *
-		 * @since 2.0.0
-		 */
-		function show_avatars() {
-			$options = get_option( 'dws_anda' );
-
-			if ( ! empty( $options['avatars'] ) ) {
-			?>
+				<?php if ( ! empty( $options['avatars'] ) ) : ?>
 				<form method="post">
 					<fieldset>
 						<legend><h3><?php esc_html_e( 'Current Custom Avatars', 'dws' ); ?></h3></legend>
 						<?php wp_nonce_field( 'dws_anda_update' ); ?>
 						<input type="hidden" name="update" />
 						<?php
-						$thumb_url = plugins_url( 'includes/timthumb.php', __FILE__ );
 						foreach ( $options['avatars'] as $avatar ) {
-							$dws_anda_name = $avatar['name'];
-							$dws_anda_local = $avatar['local'];
-							$dws_anda_url = $avatar['url'];
-							$dws_anda_uid = $avatar['uid'];
-							$image_url = $thumb_url . '?src=' . $dws_anda_local;
 							?>
-									<h4><input type="checkbox" value="<?php echo esc_attr( $dws_anda_uid ); ?>" name="dws_anda_delete[]" /> <?php echo esc_html( $dws_anda_name ); ?></h4>
-									<p><img rel="<?php echo esc_attr( $dws_anda_local ); ?>" class='new-default-avatar' src='<?php echo esc_url( $image_url ); ?>' alt='' /></p>
+							<h4><input type="checkbox" value="<?php echo esc_attr( $avatar['uid'] ); ?>" name="dws_anda_delete[]" /> <?php echo esc_html( $avatar['name'] ); ?></h4>
+							<p>
+								<?php echo wp_get_attachment_image( $avatar['id'] ); ?>
+							</p>
 							<?php
 						}
 						?>
-						<input type="submit" class="save" value="<?php esc_html_e( 'Remove Selected', 'dws' ); ?>" />
+						<?php submit_button( esc_html__( 'Remove Selected', 'dws' ), 'secondary' ); ?>
 					</fieldset>
 				</form>
+			<?php endif; ?>
+			</div>
 			<?php
-			}
 		}
 	}
 	$dws_anda = new DWS_ANDA();
